3fc2d20873b562ed26c2632f4fcbffd84454bb43
ArunabhPrakash <95112140+ArunabhPrakash@users.noreply.github.com>
2023-05-12 10:35:46 +0530

    Update inbuiltsort.cpp

--- my solutions/2.sort/inbuiltsort.cpp
+++ my solutions/2.sort/inbuiltsort.cpp
@@ -6,7 +6,7 @@ int main(){
 	int arr[]={10,9,8,6,5,4,3,2,11,16,8};
 	int n=sizeof(arr)/sizeof(int);
 	
-	sort(arr,arr+n);
+	sort(arr,arr+n);//for vector sort(a.begin(),a.end());
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<",";
 	}
