73a1528b83ec0a745ae73ffe7f57343fd119c0d9
ArunabhPrakash <95112140+ArunabhPrakash@users.noreply.github.com>
2023-05-17 23:33:24 +0530

    mountains

--- my solutions/1.array/test.cpp
+++ my solutions/1.array/test.cpp
@@ -1,32 +1,20 @@
 #include<iostream>
 #include<vector>
-#include<string>
+#include<unordered_set>
 using namespace std;
 
-//Complete this method, don't write main
-vector<string> fizzbuzz(int n){
-vector<string> ans;
-for(int i=1;i<=n;i++){
-    if(i%3==0 and i%5!=0){
-        ans.push_back("Fizz");
-    }
-    else if(i%3!=0 and i%5==0){
-        ans.push_back("Buzz");
-    }
-    else if(i%3==0 and i%5==0){
-        ans.push_back("FizzBuzz");
-    }
-    else{
-        ans.push_back(to_string(i));
-    }
-}
-return ans;	
-}
-
-int main(){
-	vector<string> ans;
-	ans=fizzbuzz(15);
-	for(string a:ans){
-		cout<<a<<" ";
+pair<int,int> Pairs(vector<int> &arr,int sum){
+	unordered_set<int> map;
+	for(int i=0;i<arr.size();i++){
+		map.insert(arr[i]);
+		int val = sum - arr[i];
+		if(map.find(val)!=map.end()){
+			cout<<"\nPair: "<<arr[i]<<","<<val;
+		}
 	}
+}
+int main(){
+	vector<int> arr = {10,5,2,3,-6,9,11};
+	int s=4;
+	Pairs(arr,s);
 }
