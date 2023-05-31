220f4ab595237c026b341e2f3deb9a6c20c90d58
ArunabhPrakash <95112140+ArunabhPrakash@users.noreply.github.com>
2023-06-01 00:04:23 +0530

    topview

--- my solutions/11.Binary Tree/test2.cpp
+++ my solutions/11.Binary Tree/test2.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
+#include<list>
+#include<unordered_map>
+#include<map>
 using namespace std;
+
 int main(){
-	cout<<5-6;
+	list<pair<int,int>> arr {{1,2},{2,2},{3,2}};
+	for(auto i:arr){
+		cout<<i.first<<" "<<i.second<<endl;
+	}
+	unordered_map<int,pair<int,int>> m;
+	m[1]={1,2};
+	cout<<m[1].first<<" "<<m[1].second;
+		map<int,pair<int,int>> m2;
+	m2[1]={1,2};
+	cout<<m2[1].first<<" "<<m2[1].second;
 }
